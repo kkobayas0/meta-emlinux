eab0c1353b7c5bf6da5877585567efc029d18491
Masami Ichikawa <masami.ichikawa@miraclelinux.com>
2024-04-03 22:46:44 +0000

    scripts/create_sbom: Update METAVAR names
    Use FILE for licenses and license-mapping options.
    
    Signed-off-by: Masami Ichikawa <masami.ichikawa@miraclelinux.com>

--- scripts/create_sbom.py
+++ scripts/create_sbom.py
@@ -224,9 +224,9 @@ def parse_options():
     parser.add_argument("--distro", dest="distro", help="debian distro name(e.g. bookworm)",
             default="bookworm", metavar="DISTRO")
     parser.add_argument("--licenses", dest="user_defined_licenses", help="license yaml file",
-            metavar="LICENSES")
+            metavar="FILE")
     parser.add_argument("--license-mapping", dest="user_defined_license_mapping", help="license mapping yaml file",
-            metavar="LICENSE_MAPPING")
+            metavar="FILE")
     parser.add_argument("--supplier", dest="supplier", help="Supplier name(e.g. company name)",
             metavar="SUPPLIER", required=True)
     parser.add_argument("--product", dest="product", help="Product name",
